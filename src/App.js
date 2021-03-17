fbd1e591f884f27dd10224d747f13b6b719cab78
Carlos Reyes <carmandoreyes93@gmail.com>
2021-03-17 13:22:29 -0600

    Works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState("");
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    axios
+      .post("https://academlo-chat.herokuapp.com/api/users/login", {
+        email: "test@test.com",
+        password: "random123",
+      })
+      .then((res) => setToken(res.data.user.token));
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      const connection = io("https://academlo-chat.herokuapp.com/", {
+        query: {
+          token,
+        },
+      });
+
+      setSocket(connection);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.emit("join", { name: "test", room: "69" }, (error) => {
+        if (error) {
+          console.error(error);
+        }
+      });
+
+      socket.on("roomData", (data) => {
+        console.log(data);
+      });
+      socket.on("message", (data) => {
+        console.log(data);
+      });
+    }
+  }, [socket]);
+
+  return <div className="App"></div>;
 }
 
 export default App;
